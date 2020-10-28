f1780334a2d18c96bcc45637e28ad73100cfd81d
Ramesh kumar <rameshkumar160196@gmail.com>
2020-10-29 02:48:50 +0530

    port changed for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router =require("./routes/index")
 const app=express();
 // app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-const port=8080;
+const port=process.env.PORT||8080;
 
 
 
